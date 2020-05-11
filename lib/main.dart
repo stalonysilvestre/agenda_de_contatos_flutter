8009d53d67f8e4055c8db6206172327de6d3a3a7
stalonysilvestre <56941680+stalonysilvestre@users.noreply.github.com>
2020-05-11 15:46:57 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:agenda_de_contatos_flutter/ui/home_page.dart';
+
+
+void main() { 
+  runApp(MaterialApp(
+  home: HomePage(),
+  debugShowCheckedModeBanner: false,
+));
+}
+
+
